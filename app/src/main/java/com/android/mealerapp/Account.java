4ad8919a7de9aede9390e1f5f3f4def3643c4437
Jenny-Carl <95900989+Jenny-Carl@users.noreply.github.com>
2022-10-18 18:10:48 -0400

    Adding Account superclass

---
+++ app/src/main/java/com/android/mealerapp/Account.java
@@ -0,0 +1,20 @@
+package com.android.mealerapp;
+
+public class Account {
+
+    protected String name, lastName, email, pswd, address;
+
+    public Account(){
+
+    }
+    
+    public Account(String name, String lastName, String email, String pswd, String address){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.pswd = pswd;
+        this.address = address;
+    }
+
+
+}
