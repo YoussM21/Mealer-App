a45bf5d41cd2dcd814278dd63a16c81d77e3e586
Youssef Mroue <mroueh.youssef10@gmail.com>
2022-10-22 00:02:32 -0400

    Fix button function and Payment Info page

--- app/src/main/java/com/android/mealerapp/ClientSignUpActivity.java
+++ app/src/main/java/com/android/mealerapp/ClientSignUpActivity.java
@@ -2,7 +2,9 @@ package com.android.mealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClientSignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ClientSignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_signup);
+    }
+
+    public void onNextClick(View view) {
+        Intent intent1 = new Intent(getApplicationContext(), PaymentInfoActivity.class);
+        startActivity(intent1);
     }
 }
