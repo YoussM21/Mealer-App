161bdf49f520c8fc32ca34e24cefb59013cad33e
Christopher Krayem <114031156+ChrisKrayem@users.noreply.github.com>
2022-11-14 22:23:28 -0500

    cook pages

--- app/src/main/java/com/android/mealerapp/CookWelcomePage.java
+++ app/src/main/java/com/android/mealerapp/CookWelcomePage.java
@@ -4,14 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,7 +15,7 @@ public class CookWelcomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cook_welcome_screen);
+        setContentView(R.layout.suspended_cook_screen);
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         DatabaseReference usersDatabase = FirebaseDatabase.getInstance().getReference("Users");
