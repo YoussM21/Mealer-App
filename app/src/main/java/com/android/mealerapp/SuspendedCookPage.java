dfbc5633c9ed116a619e556150e2f2a5c5b9352d
Christopher Krayem <114031156+ChrisKrayem@users.noreply.github.com>
2022-11-15 19:09:41 -0500

    java classes for the 2 different cook pages

--- app/src/main/java/com/android/mealerapp/SuspendedCookPage.java
+++ app/src/main/java/com/android/mealerapp/SuspendedCookPage.java
@@ -11,22 +11,18 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class CookWelcomePage extends AppCompatActivity {
+public class SuspendedCookPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.suspended_cook_screen);
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         DatabaseReference usersDatabase = FirebaseDatabase.getInstance().getReference("Users");
 
     }
 
-    public void onNewMealClick(View view){
-        Intent intent2 = new Intent(getApplicationContext(), LogInActivity.class);
-        startActivity(intent2);
-    }
-
-    public void onLogoutClick_3(View view){
+    public void onLogoutClick_2(View view){
         FirebaseAuth.getInstance().signOut();
         Intent intent1 = new Intent(getApplicationContext(), rolePage.class);
         startActivity(intent1);
