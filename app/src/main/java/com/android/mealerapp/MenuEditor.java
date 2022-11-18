10ad185e6634c438b560802834b9ef57766e2122
ChrisKrayem <christopherkrayem@gmail.com>
2022-11-18 18:36:06 -0500

    recommend checkbox added

--- app/src/main/java/com/android/mealerapp/MenuEditor.java
+++ app/src/main/java/com/android/mealerapp/MenuEditor.java
@@ -1,30 +1,44 @@
 package com.android.mealerapp;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 
-import java.util.List;
-
 public class MenuEditor extends AppCompatActivity {
 
     EditText editTextMealName;
     EditText editTextMeal_Description;
+    CheckBox Recommend_Button;
     Button buttonAddMeal;
 
     DatabaseReference databaseMeals;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_meal_page);
+
+        editTextMealName = findViewById(R.id.editTextMealName);
+        editTextMeal_Description = findViewById(R.id.editTextMeal_Description);
+        Recommend_Button = findViewById(R.id.Recommend_button);
+        buttonAddMeal = findViewById(R.id.Addbutton);
+
+        //adding an onclick listener to button
+        buttonAddMeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addProduct();
+            }
+        });
+    }
 
     private void addProduct() {
 
@@ -43,24 +57,6 @@ public class MenuEditor extends AppCompatActivity {
         else {
             Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
         }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_meal_page);
-
-        editTextMealName = findViewById(R.id.editTextMealName);
-        editTextMeal_Description = findViewById(R.id.editTextMeal_Description);
-        buttonAddMeal = findViewById(R.id.Addbutton);
-
-        //adding an onclick listener to button
-        buttonAddMeal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addProduct();
-            }
-        });
     }
 }
 
