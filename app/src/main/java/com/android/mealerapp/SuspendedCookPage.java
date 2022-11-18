498f3377bf62f425c354f4bcf7067db6821852d5
ChrisKrayem <christopherkrayem@gmail.com>
2022-11-18 18:15:11 -0500

    Update SuspendedCookPage.java

--- app/src/main/java/com/android/mealerapp/SuspendedCookPage.java
+++ app/src/main/java/com/android/mealerapp/SuspendedCookPage.java
@@ -3,6 +3,7 @@ package com.android.mealerapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SuspendedCookPage extends AppCompatActivity {
+
+    TextView textView_Suspension_length;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,8 @@ public class SuspendedCookPage extends AppCompatActivity {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         DatabaseReference usersDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+        textView_Suspension_length = findViewById(R.id.textView_Suspension_length);
 
     }
 
