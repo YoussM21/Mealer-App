62ca92ac41452839a4eda9b3d439a671ea8ffbf5
Christopher Krayem <114031156+ChrisKrayem@users.noreply.github.com>
2022-12-06 18:55:24 -0500

    extra client ui pages

--- app/src/main/java/com/android/mealerapp/CookWelcomePage.java
+++ app/src/main/java/com/android/mealerapp/CookWelcomePage.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
