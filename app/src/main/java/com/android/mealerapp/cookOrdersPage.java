787c41e5d928dac9f32477acda21863808aad271
Christopher Krayem <114031156+ChrisKrayem@users.noreply.github.com>
2022-12-07 22:14:28 -0500

    added log out function on cook orders page

--- app/src/main/java/com/android/mealerapp/cookOrdersPage.java
+++ app/src/main/java/com/android/mealerapp/cookOrdersPage.java
@@ -1,8 +1,11 @@
 package com.android.mealerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class cookOrdersPage extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class cookOrdersPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cook_orders_page);
+    }
+
+    public void onLogoutClick_5(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent1 = new Intent(getApplicationContext(), rolePage.class);
+        startActivity(intent1);
     }
 }
